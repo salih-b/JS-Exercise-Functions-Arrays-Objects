170f1cb860d854b0df0dc8499ed5ebc54906fd03
Salih Babekr <salihmorta17@gmail.com>
2020-01-27 23:21:10 -0500

    completed set up and first problem

--- index.js
+++ index.js
@@ -39,8 +39,14 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
+function makePersonObject(id, name, email) {
   /* code here */
+  var makePersonObject = {
+    id: id,
+    name: name, 
+    email: email
+  }
+  return makePersonObject;
 }
 
 /**
